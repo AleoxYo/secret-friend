6b6279eb415472ecc780e13e2208eb8e9f9a9e5c
AleoxYo <eidalcas16@gmail.com>
2025-01-23 16:04:06 -0500

    Creacion de funcion sortear amigos y final de la logica del proyecto

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ let addedFriends = [];
 
 const inputName = document.querySelector(".input-name");
 const buttonAdd = document.querySelector(".button-add");
+const friendsList = document.querySelector(".name-list");
 
 const addFriend = () => {
   let value = inputName.value;
@@ -12,11 +13,27 @@ const addFriend = () => {
     alert("Por favor, inserte un nombre");
   }
   inputName.value = "";
+  updateFriendsList(addedFriends);
   console.log(addedFriends);
+};
+
+const updateFriendsList = (friends) => {
+  friendsList.innerHTML = "";
+  for (const name of friends) {
+    friendsList.innerHTML = friendsList.innerHTML + `<li>${name}</li>`;
+  }
+};
+
+const sortFriend = (friends) => {
+  console.log(friends);
+  if (addedFriends) {
+    const sortedFriendPosition = Math.floor(Math.random() * friends.length);
+    document.querySelector(
+      ".button-draw"
+    ).innerHTML = `El amigo secreto es ${friends[sortedFriendPosition]}`;
+  }
 };
 
 // const test = inputName.addEventListener("change", () =>
 //   console.log(inputName.value)
 // );
-
-console.log(inputName);
