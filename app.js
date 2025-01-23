ae1fa1f5b3ccc5d7d0392e9217e11862d862fd01
AleoxYo <eidalcas16@gmail.com>
2025-01-23 00:39:30 -0500

    Creacion de la funcion addFriend

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let addedFriends = [];
+
+const inputName = document.querySelector(".input-name");
+const buttonAdd = document.querySelector(".button-add");
+
+const addFriend = () => {
+  let value = inputName.value;
+  if (value) {
+    addedFriends.push(value);
+  } else {
+    alert("Por favor, inserte un nombre");
+  }
+  inputName.value = "";
+  console.log(addedFriends);
+};
+
+// const test = inputName.addEventListener("change", () =>
+//   console.log(inputName.value)
+// );
+
+console.log(inputName);
